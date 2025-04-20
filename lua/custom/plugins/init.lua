87859b2e885dc1d42fbd455b34a5d95866eec9fd
alma <kovacs.erik.hu@gmail.com>
2025-04-20 03:30:18 +0200

    first commit

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,19 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      open_mapping = [[<C-\>]],
+    },
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+  },
+}
